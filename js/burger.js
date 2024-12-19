b621314cf26a53a698a0916dfab7aecebb4e7594
Светлана Каверина <lana.kaverina@internet.ru>
2024-12-19 15:53:33 +0300

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,4 @@
+// function menuOnClick(){
+//     document.querySelector('.nav__list').classList.toggle('nav__list--vertical');
+//     document.getElementById('menu-bg').classList.toggle('change-bg')
+// }
